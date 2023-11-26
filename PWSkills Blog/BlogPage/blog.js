b53c33985b07a9ae4120528bdb9fc6e16325c786
kushagrakatiha <kushagrakatiha123@gmail.com>
2023-11-26 19:19:50 +0530

    routing done

---
+++ PWSkills Blog/BlogPage/blog.js
@@ -0,0 +1,5 @@
+const backBtn = document.querySelector('.back-arrow');
+
+backBtn.addEventListener('click', () => {
+    window.location.href = '../HomePage/index.html';
+});
--- PWSkills Blog/HomePage/script.js
+++ PWSkills Blog/HomePage/script.js
@@ -1,6 +1,13 @@
 const blogModel = document.querySelector('.form-cover');
 const addBlogBtn = document.querySelector('.plus');
 const backBtn = document.querySelector('.back-arrow');
+const readBtnArr = document.querySelectorAll('.read');
+
+readBtnArr.forEach(element => {
+    element.addEventListener('click', () => {
+        window.location.href = '../BlogPage/blog.html'
+    });
+});
 
 addBlogBtn.addEventListener('click', () => {
     blogModel.style.visibility = 'visible';
