b4df85bb1c51f429628153fa7cd5325823deb22a
kushagrakatiha <kushagrakatiha123@gmail.com>
2023-11-27 19:17:18 +0530

    del and add blog completed

--- PWSkills Blog/Client/HomePage/script.js
+++ PWSkills Blog/Client/HomePage/script.js
@@ -25,68 +25,76 @@ const blogModelDesc = document.querySelector('.blog-form-desc');
 const blogModelContent = document.querySelector('.blog-form-content');
 const blogModelBtn = document.querySelector('.blog-form-submit');
 
-// Making function to add blog form data to local storage
+// Making function to add blog form data to blog array
 
 const addBlog = (e) => {
+    
+    let blogArr = JSON.parse(localStorage.getItem('blog')) || [];
     e.preventDefault();
+    
     let blogData = {
         img: blogModelImg.value,
         title: blogModelTitle.value,
         desc: blogModelDesc.value,
         content: blogModelContent.value
     }
-    console.log(blogData);
-    localStorage.setItem('blog', JSON.stringify(blogData));
+    
+    blogArr.push(blogData);
+    localStorage.setItem('blog', JSON.stringify(blogArr));
+
+    
     blogModel.style.visibility = 'hidden';
+    
     window.location.reload();
+    
 }
 
 blogModelBtn.addEventListener('click', addBlog);
 
-// Fetching blog data from local storage and setting it to blog card
+// Fetching blogs from blog array and returning blog cards to client
 
-const blogCardImg = document.querySelector('.blog-image');
-const blogCardTitle = document.querySelector('.blog-title');
-const blogCardDesc = document.querySelector('.blog-desc');
+const blogArrLocal = JSON.parse(localStorage.getItem('blog'));
+console.log(blogArrLocal);
 
-// write a function to fetch data from local storage and set it to blog card
+const blogContainer = document.querySelector('.blog-container');
 
 const setBlogData = () => {
-    
 
-    let blogData = JSON.parse(localStorage.getItem('blog'));
+    if (blogArrLocal.length > 0) {
 
-    if (blogData) {
+        blogArrLocal.forEach(element => {
 
-        document.querySelector('.blog-container').innerHTML = `\
-   
-        <div class="blog-card">
-            <img class="blog-image" src = ${blogData.img}alt=""/>
-            <div class="blog-title">${blogData.title}</div>
-            <div class="blog-desc">${blogData.desc}</div>
-            <button class="rd-btn">Read</button>
-            <button class="del-btn">Delete</button>
-        </div>`
+            blogContainer.innerHTML += `
+            <div class="blog-card">
+                <img class="blog-image" src = ${element.img}alt=""/>
+                <div class="blog-title">${element.title}</div>
+                <div class="blog-desc">${element.desc}</div>
+                <button class="rd-btn">Read</button>
+                <button class="del-btn">Delete</button>
+            </div>`
 
-        console.log(blogData);
+        });
 
-
-    }else {
-        // document.querySelector('.blog-card').style.display = 'none';
-        document.querySelector('.blog-container').innerHTML = '<h1 class="no-blog">No Blog Found</h1>';
-    }
+    } else {
+        document.querySelector('.blog-container').innerHTML = '<h1 class="no-blog">No Blog Found !!</h1>';    
 }
-
+}
 setBlogData();
 
-// Deleting blog data from local storage
+// Deleting blog card from blog array and local storage 
 
-if(document.querySelector('.del-btn')){
-    document.querySelector('.del-btn').addEventListener('click', () => {
-        localStorage.removeItem('blog');
-        document.querySelector('.blog-container').innerHTML = '<h1 class="no-blog">No Blog Found</h1>';
+const delBtnArr = document.querySelectorAll('.del-btn');
+
+delBtnArr.forEach(element => {
+    element.addEventListener('click', () => {
+        let blogArr = JSON.parse(localStorage.getItem('blog'));
+        blogArr.splice(blogArr.indexOf(element), 1);
+        localStorage.setItem('blog', JSON.stringify(blogArr));
+        window.location.reload();
     });
-}
+});
+
+
 
 
 
