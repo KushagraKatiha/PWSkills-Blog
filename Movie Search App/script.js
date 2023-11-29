c87f90f5b42beb5dd139f0628563f742d6afb383
kushagrakatiha <kushagrakatiha123@gmail.com>
2023-11-30 00:12:49 +0530

    movie app completed

--- Movie Search App/script.js
+++ Movie Search App/script.js
@@ -1,8 +1,53 @@
-let movieData = () => {
-    fetch("http://www.omdbapi.com/?t=Batman&apikey=7b42929a")
-    .then(response => response.json())
-    .then(data => console.log(data))
-    .catch(err => console.log(err))
-}
+// Taking user input from the web page 
+const searchField = document.querySelector(".search")
+const searchButton = document.querySelector(".searchButton")
+const movieCard = document.querySelector(".movieCard")
+const movieBox = document.querySelector(".movies")
+// Event listener for the search button
 
-movieData()
+searchButton.addEventListener("click", async (e) => {
+    e.preventDefault()
+    let searchValue = searchField.value
+    if(searchValue == ""){
+        return
+    }
+    console.log(searchValue);
+
+    let movieDetails = await movieData(searchValue)
+    console.log(movieDetails);
+
+    searchField.value = ""
+
+    // Setting data from the API to the movie card
+    if(movieDetails.Response == "False"){
+        if(document.querySelector('.searchHeading')){
+            document.querySelector('.searchHeading').style.display = "none"}
+        movieBox.innerHTML = `<h2 class="noMovieAlert">No Movie Found !!<h2>`
+    }else{
+        if(document.querySelector('.searchHeading')){
+        document.querySelector('.searchHeading').style.display = "none"}
+        if(document.querySelector('.noMovieAlert')){
+        document.querySelector('.noMovieAlert').style.display = "none"}
+        
+        movieBox.innerHTML += `
+        <div class="movieCard">
+            <img src="${movieDetails.Poster}" alt="Movie Poster">
+            <h3 class="movieTitle">${movieDetails.Title}</h3>
+            <button class="cardBtn">Watch Now</button>
+        </div>
+        `
+    }
+})
+
+// Function to fetch the movie data from the API
+
+async function movieData (searchValue){
+    let movieName = searchValue.split(" ").join("+")
+    try {
+        const response = await fetch(`http://www.omdbapi.com/?t=${movieName}&apikey=7b42929a`)
+        const data = await response.json()
+        return data
+    } catch (err) {
+        return "Movie Not Found"
+    }
+}
