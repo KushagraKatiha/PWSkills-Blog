cd18453817906174b9cc3363b235ad8e7cc80504
kushagrakatiha <kushagrakatiha123@gmail.com>
2023-12-01 00:49:44 +0530

    componets initiated

--- ToDo - React/src/App.jsx
+++ ToDo - React/src/App.jsx
@@ -1,10 +1,17 @@
+import AddTodo from "./Components/AddTodo"
+import CardTodo from "./Components/CardTodo"
 
 function App() {
 
   return (
     <>
       <div className="w-screen h-screen bg-cyan-950">
-        
+          <div>
+            <AddTodo/>
+          </div>
+          <div>
+            <CardTodo/>
+          </div>
       </div>
     </>
   )
