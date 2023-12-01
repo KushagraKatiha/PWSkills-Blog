5544bd0f06530730e9e34467395e79710fb5c925
kushagrakatiha <kushagrakatiha123@gmail.com>
2023-12-01 15:35:01 +0530

    dummy card created

--- ToDo - React/src/Components/CardTodo.jsx
+++ ToDo - React/src/Components/CardTodo.jsx
@@ -3,8 +3,11 @@ import React from 'react'
 function CardTodo() {
   return (
     // CardTodo component
-    <div className='text-center bg-black h-[30%] w-20 m-auto mt-32'>
-        Hello
+    <div className='text-left p-4 rounded text-white bg-black h-80 w-56 m-auto mt-32'>
+        <h2 className='mb-10'>1. Learn useRef</h2>
+        <h2 className='mb-10'>Status: Pending</h2>
+        <button className='bg-purple-600 block rounded w-full py-2 mb-4 mt-20'>Update Status</button>
+        <button className='bg-purple-600 block rounded w-full py-2 mb-4 '>Remove</button>
     </div>
   )
 }
